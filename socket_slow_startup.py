f1b6033928caa342606e2a991a63a60adc5c71c1
jolinhuang <xsjcy2bhzl@gmail.com>
2013-01-13 14:21:08 +0800

    modify

--- socket_slow_startup.py
+++ socket_slow_startup.py
@@ -17,9 +17,10 @@ def createServer():
 		try:
 			print 'Wating for data'
 			tcpAccept=tcp.accept()
-			#print 'get data from :%s'%fromAddr
+			remoteData=tcpAccept[0]
+			print 'get data from :%s'%tcpAccept[1]
 			while True:
-				data=tcpAccept.recv(BUFSIZE)
+				data=remoteData.recv(BUFSIZE)
 				if not data:
 					break
 				tcp.send('[%s]%s'%(ctime()),'recvied')
