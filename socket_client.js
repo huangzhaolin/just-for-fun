d4afbd33751cd7772af0021ce17349471c825b1d
jolinhuang <xsjcy2bhzl@gmail.com>
2013-01-13 14:13:26 +0800

    modify

--- socket_client.js
+++ socket_client.js
@@ -5,9 +5,10 @@ var socket_client=new net.Socket();
 socket_client.setKeepAlive(true);
 socket_client.connect(65535,"localhost",function(){
 	console.log('start to send data info');
-	fs.readFile('/tmp/big.data',function(err,data){
-		socket_client.write(data);
-	});
+	socket_client.write("data");
+	// fs.readFile('/tmp/big.data',function(err,data){
+	// 	socket_client.write(data);
+	// });
 });
 socket_client.on('close',function(){
 	console.log('close!');
