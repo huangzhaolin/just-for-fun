f009c51476511a2b973ecd530fbadb241f405cc0
jolinhuang <xsjcy2bhzl@gmail.com>
2013-01-13 14:03:20 +0800

    modify

--- socket_slow_startup.py
+++ socket_slow_startup.py
@@ -5,24 +5,24 @@
 from socket import *
 from time import ctime
 
-HOST='localhost'
-PORT=65535
-BUFSIZE=1024
-ADDR=(HOST,PORT)
-
-tcp=socket(AF_INET,SOCK_STREAM)
-tcp.bind(ADDR)
-tcp.listen(1)
-
-while True:
-	print 'Wating for data'
-	tcp,fromAddr=tcp.accept()
-	print 'get data from :%s'%fromAddr
+def createServer():
+	HOST='localhost'
+	PORT=65535
+	BUFSIZE=1024
+	ADDR=(HOST,PORT)
+	tcp=socket(AF_INET,SOCK_STREAM)
+	tcp.bind(ADDR)
+	tcp.listen(1)
 	while True:
-		data=tcp.recv(BUFSIZE)
-		if not data:
-			break
-		tcp.send('[%s]%s'%(ctime()),'recvied')
-
-		#tcp.close()
-tcp.close()
+		print 'Wating for data'
+		tcp,fromAddr=tcp.accept()
+		print 'get data from :%s'%fromAddr
+		while True:
+			data=tcp.recv(BUFSIZE)
+			if not data:
+				break
+			tcp.send('[%s]%s'%(ctime()),'recvied')
+			#tcp.close()
+	tcp.close()
+if __name__ == '__main__':
+	createServer()
