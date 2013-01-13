31c53e0cdf8973231f1ea9871cbf23655302b674
jolinhuang <xsjcy2bhzl@gmail.com>
2013-01-13 13:44:01 +0800

    modify

--- socket_client.js
+++ socket_client.js
@@ -1,14 +1,14 @@
 //测试TCP的慢启动,内容为<HTTP权威指南> P89 客户端
 var net = require('net');
 var fs=require('fs');
-var socket=new net.Socket();
-socket.setKeepAlive(true);
-socket.connect(65535,"localhost",function(){
+var socket_client=new net.Socket();
+socket_client.setKeepAlive(true);
+socket_client.connect(65535,"localhost",function(){
 	console.log('start to send data info');
 	fs.readFile('tmp/big.data',function(err,data){
-		socket.write(data);
+		socket_client.write(data);
 	});
 }
-socket.on('close',function(){
+socket_client.on('close',function(){
 	console.log('close!');
 })
